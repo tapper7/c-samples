d9cb1a397556f2c4eaef7770910cf8eda2586b58
Chris "Tapper" Welke <chris@tapper7.com>
2016-09-20 12:52:57 -0700

    GoesTo.c
    uses the mythical (& non-existant) "goes-to" operator, but actually works
    as shown in the code, this is dependent on stdio.h to work.

---
+++ GoesTo.c
@@ -0,0 +1,10 @@
+//ex: operator-->
+#include <stdio.h> 
+int main(){ 
+    int x = 10; 
+    while(x-->0)//goesTO operator
+    printf("%d ",x);
+}//output should be "9 8 7 6 5 4 3 2 1" lulz
+    //try using the extreme reverse GoesTo operator if you dare....
+    //printf("%d ",x/10);
+    //while(0<--------------------x)
